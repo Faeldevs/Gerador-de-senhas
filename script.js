92329a699edac78559b3775639ffee25ea1264ec
Rafael S. Silva <103973828+Faeldevs@users.noreply.github.com>
2022-10-22 00:50:58 -0300

    Projeto Concluído
    Gerador de senhas aleatória usando o básico de Java script.

---
+++ script.js
@@ -0,0 +1,35 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+  let pass ="";
+  
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+  pass += charset.charAt(Math.floor(Math.random() * n));
+  
+  } 
+  
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copypassword(){
+    
+    navigator.clipboard.writeText(novaSenha);
+    
+}
